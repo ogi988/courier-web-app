becaaf78dfdd880b89a8379ed4c219f6854c5551
Ognjen <boskovic.ogi1998@gmail.com>
2019-05-19 16:26:30 +0200

    sveeeee

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->integer('type')->nullable();//1-fizicko,2-pravno
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
